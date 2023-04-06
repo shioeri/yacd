ebac5f44cf56f30551e2039e39ffc6cd00c8f0ac
hinak0 <hinak0@qq.com>
2023-04-06 19:08:24 +0800

    feature: censor single ip address's connections
    (cherry picked from commit dec36c6646a2331786b1c779393b2fb91f9a7ab6)
    
    [fix] fix a performance bug
    
    (cherry picked from commit 8cd1759ad8e8259523b993e4e5213ff9d64deb74)

--- src/components/Connections.tsx
+++ src/components/Connections.tsx
@@ -12,6 +12,7 @@ import { ClashAPIConfig } from '$src/types';
 import * as connAPI from '../api/connections';
 import useRemainingViewPortHeight from '../hooks/useRemainingViewPortHeight';
 import { getClashAPIConfig } from '../store/app';
+import Button from './Button';
 import s from './Connections.module.scss';
 import ConnectionTable from './ConnectionTable';
 import { MutableConnRefCtx } from './conns/ConnCtx';
@@ -61,23 +62,38 @@ type FormattedConn = {
 function hasSubstring(s: string, pat: string) {
   return (s ?? '').toLowerCase().includes(pat.toLowerCase());
 }
+function filterConnIps(conns: FormattedConn[], ipStr: string) {
+  return conns.filter((each) => each.sourceIP === ipStr);
+}
 
-function filterConns(conns: FormattedConn[], keyword: string) {
-  return !keyword
-    ? conns
-    : conns.filter((conn) =>
-        [
-          conn.host,
-          conn.sourceIP,
-          conn.sourcePort,
-          conn.destinationIP,
-          conn.chains,
-          conn.rule,
-          conn.type,
-          conn.network,
-          conn.processPath,
-        ].some((field) => hasSubstring(field, keyword)),
-      );
+function filterConns(conns: FormattedConn[], keyword: string, sourceIp: string) {
+  let result = conns;
+  if (keyword !== '') {
+    result = conns.filter((conn) =>
+      [
+        conn.host,
+        conn.sourceIP,
+        conn.sourcePort,
+        conn.destinationIP,
+        conn.chains,
+        conn.rule,
+        conn.type,
+        conn.network,
+        conn.processPath,
+      ].some((field) => {
+        return hasSubstring(field, keyword);
+      })
+    );
+  }
+  if (sourceIp !== '') {
+    result = filterConnIps(result, sourceIp);
+  }
+  // result.forEach((e) => console.log(e.sourceIP));
+  return result;
+}
+
+function getConnIpList(conns: FormattedConn[]) {
+  return Array.from(new Set(conns.map((x) => x.sourceIP))).sort();
 }
 
 function formatConnectionDataItem(
@@ -131,11 +147,19 @@ function connQty({ qty }) {
 
 function Conn({ apiConfig }: { apiConfig: ClashAPIConfig }) {
   const [refContainer, containerHeight] = useRemainingViewPortHeight();
+
   const [conns, setConns] = useState([]);
   const [closedConns, setClosedConns] = useState([]);
+
   const [filterKeyword, setFilterKeyword] = useState('');
-  const filteredConns = filterConns(conns, filterKeyword);
-  const filteredClosedConns = filterConns(closedConns, filterKeyword);
+  const [filterSourceIpStr, setFilterSourceIpStr] = useState('');
+
+  const filteredConns = filterConns(conns, filterKeyword, filterSourceIpStr);
+  const filteredClosedConns = filterConns(closedConns, filterKeyword, filterSourceIpStr);
+
+  const connIpSet = getConnIpList(conns);
+  const ClosedConnIpSet = getConnIpList(closedConns);
+
   const [isCloseAllModalOpen, setIsCloseAllModalOpen] = useState(false);
   const openCloseAllModal = useCallback(() => setIsCloseAllModalOpen(true), []);
   const closeCloseAllModal = useCallback(() => setIsCloseAllModalOpen(false), []);
@@ -172,8 +196,9 @@ function Conn({ apiConfig }: { apiConfig: ClashAPIConfig }) {
         prevConnsRef.current = x;
       }
     },
-    [setConns, isRefreshPaused, connCtx],
+    [isRefreshPaused, connCtx]
   );
+
   useEffect(() => {
     return connAPI.fetchData(apiConfig, read);
   }, [apiConfig, read]);
@@ -220,7 +245,17 @@ function Conn({ apiConfig }: { apiConfig: ClashAPIConfig }) {
             }}
           >
             <TabPanel>
-              <>{renderTableOrPlaceholder(filteredConns)}</>
+              <Button onClick={() => setFilterSourceIpStr('')} kind="minimal">
+                {t('All')}
+              </Button>
+              {connIpSet.map((value, k) => {
+                return (
+                  <Button key={k} onClick={() => setFilterSourceIpStr(value)} kind="minimal">
+                    {value}
+                  </Button>
+                );
+              })}
+              {renderTableOrPlaceholder(filteredConns)}
               <Fab
                 icon={isRefreshPaused ? <Play size={16} /> : <Pause size={16} />}
                 mainButtonStyles={isRefreshPaused ? { background: '#e74c3c' } : {}}
@@ -233,7 +268,19 @@ function Conn({ apiConfig }: { apiConfig: ClashAPIConfig }) {
                 </Action>
               </Fab>
             </TabPanel>
-            <TabPanel>{renderTableOrPlaceholder(filteredClosedConns)}</TabPanel>
+            <TabPanel>
+              <Button onClick={() => setFilterSourceIpStr('')} kind="minimal">
+                {t('All')}
+              </Button>
+              {ClosedConnIpSet.map((value, k) => {
+                return (
+                  <Button key={k} onClick={() => setFilterSourceIpStr(value)} kind="minimal">
+                    {value}
+                  </Button>
+                );
+              })}
+              {renderTableOrPlaceholder(filteredClosedConns)}
+            </TabPanel>
           </div>
         </div>
         <ModalCloseAllConnections
