f30fba2b1407d381ce247ad01a2da495ed924fd6
rookisbusy <rookisbusy@gmail.com>
2023-04-07 16:58:31 +0800

    feat: inspect memory on dashboard

--- src/components/TrafficNow.tsx
+++ src/components/TrafficNow.tsx
@@ -21,7 +21,7 @@ export default connect(mapState)(TrafficNow);
 function TrafficNow({ apiConfig }: { apiConfig: ClashAPIConfig }) {
   const { t } = useTranslation();
   const { upStr, downStr } = useSpeed(apiConfig);
-  const { upTotal, dlTotal, connNumber } = useConnection(apiConfig);
+  const { upTotal, dlTotal, connNumber, mTotal } = useConnection(apiConfig);
   return (
     <div className={s0.TrafficNow}>
       <div className={s0.sec}>
@@ -43,6 +43,10 @@ function TrafficNow({ apiConfig }: { apiConfig: ClashAPIConfig }) {
       <div className={s0.sec}>
         <div>{t('Active Connections')}</div>
         <div>{connNumber}</div>
+      </div>
+      <div className={s0.sec}>
+        <div>{t('Memory Total')}</div>
+        <div>{mTotal}</div>
       </div>
     </div>
   );
@@ -66,13 +70,15 @@ function useConnection(apiConfig: ClashAPIConfig) {
     upTotal: '0 B',
     dlTotal: '0 B',
     connNumber: 0,
+    mTotal: '0 B',
   });
   const read = useCallback(
-    ({ downloadTotal, uploadTotal, connections }) => {
+    ({ downloadTotal, uploadTotal, connections, memory }) => {
       setState({
         upTotal: prettyBytes(uploadTotal),
         dlTotal: prettyBytes(downloadTotal),
         connNumber: connections.length,
+        mTotal: prettyBytes(memory),
       });
     },
     [setState],
